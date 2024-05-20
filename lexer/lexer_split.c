4086c1f581087182da27bcfeadada64f7642e162
Soufyane Marsi <smarsi@c4r5p8.1337.ma>
2024-05-20 15:20:31 +0100

    mnk leaks akhay chihab

--- lexer/lexer_split.c
+++ lexer/lexer_split.c
@@ -6,7 +6,7 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 06:00:18 by smarsi            #+#    #+#             */
-/*   Updated: 2024/05/19 18:43:46 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/05/20 10:22:01 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,7 +109,9 @@ char	**lexer_split(char *s, char *delimiters)
 	int		count;
 
     if (!s || !delimiters)
-        return (NULL);
+	{
+    	return (NULL);
+	}
 	count = count_word(s, delimiters);
 	dst = ft_calloc(count + 1, sizeof(char *));
 	if (!dst)
--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:03:52 by smarsi            #+#    #+#             */
-/*   Updated: 2024/05/19 18:37:14 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/05/20 10:20:26 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_lstdelone(t_lexer *lst)
 	if (lst)
 	{
 		free(lst->value);
+		lst->value = NULL;
 		free(lst);
 		lst = NULL;
 	}
