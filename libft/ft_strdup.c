23e7326780ccf985a851e87bd74d41cb53ff5c8b
Soufyane Marsi <smarsi@c3r7p4.1337.ma>
2024-07-25 12:27:32 +0100

    libft norminette

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,26 +6,26 @@
 /*   By: smarsi <smarsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:57:17 by smarsi            #+#    #+#             */
-/*   Updated: 2024/05/23 09:57:10 by smarsi           ###   ########.fr       */
+/*   Updated: 2024/07/25 12:27:09 by smarsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-    char *dest;
-    size_t i;
-    
-    dest = (char *) malloc(ft_strlen(s1) + 1);
-    if (!dest)
-        return (ft_strdup(""));
-    i = 0;
-    while (s1[i])
-    {
-        dest[i] = s1[i];
-        i++;
-    }
-    dest[i] = 0;
-    return (dest);
+	char	*dest;
+	size_t	i;
+
+	dest = (char *) malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (ft_strdup(""));
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
 }
