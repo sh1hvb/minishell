367a4dd7e94cba9286478b25302c352a7277edda
Sh1hvb <medchihab651@protonmail.com>
2024-07-25 17:44:55 +0100

    fix ft_unset.c to not lost the head

--- execution/ft_unset.c
+++ execution/ft_unset.c
@@ -1,35 +1,44 @@
 #include "../minishell.h"
 
 
-void ft_unset(t_data *data)
+void	ft_unset(t_data *data)
 {
-    int i = 1;
-    t_envp *head;
+	int		i;
+	t_envp	*head;
+	t_envp	*tmp;
 
-    head = env;
-    while (data->args[i])
-    {
-        while (head)
-        {
-            if(!ft_strcmp(data->args[i], head->key) )
-            {
-                if(!head->prev)
-                {
-                    env = env->next;
-                    env->prev = NULL;
-                    free(head->key);
-                    free(head->value);
-                    free(head);
-                    break;
-                }
-                head->prev->next = head->next;
-                free(head->key);
-                free(head->value);
-                head = env;
-                break;
-            }
-            head = head->next;
-        }
-        i++;
-    }
+	i = 1;
+	while (data->args[i])
+	{
+		head = env;
+		while (head)
+		{
+			if (!ft_strcmp(data->args[i], head->key))
+			{
+				if (!head->prev)
+				{
+					env = head->next;
+					if (env)
+						env->prev = NULL;
+				}
+				else
+				{
+					head->prev->next = head->next;
+					if (head->next)
+						head->next->prev = head->prev;
+				}
+				free(head->key);
+				free(head->value);
+				tmp = head;
+				head = head->next;
+				free(tmp);
+				break ;
+			}
+			else
+			{
+				head = head->next;
+			}
+		}
+		i++;
+	}
 }
